69245c2cb95bacfe73f0f7c9b6b90da62b9e93d0
ITesserakt <potryas85@mail.ru>
2022-06-25 04:17:23 +0300

    fix: redundant strict comparison of expressions

--- Traversal/src/main/kotlin/ru/tesserakt/kodept/traversal/inference/TypeInferenceAnalyzer.kt
+++ Traversal/src/main/kotlin/ru/tesserakt/kodept/traversal/inference/TypeInferenceAnalyzer.kt
@@ -3,8 +3,6 @@ package ru.tesserakt.kodept.traversal.inference
 import arrow.core.continuations.eagerEffect
 import arrow.core.identity
 import arrow.core.nel
-import arrow.core.nonEmptyListOf
-import mu.KotlinLogging
 import ru.tesserakt.kodept.core.AST
 import ru.tesserakt.kodept.core.Tree
 import ru.tesserakt.kodept.core.walkDownTop
@@ -14,17 +12,10 @@ import ru.tesserakt.kodept.error.SemanticError
 import ru.tesserakt.kodept.error.SemanticNote
 import ru.tesserakt.kodept.traversal.*
 import ru.tesserakt.kodept.traversal.inference.Type.*
-import ru.tesserakt.kodept.traversal.inference.Type.Companion.bool
-import ru.tesserakt.kodept.traversal.inference.Type.Companion.char
-import ru.tesserakt.kodept.traversal.inference.Type.Companion.floating
-import ru.tesserakt.kodept.traversal.inference.Type.Companion.number
-import ru.tesserakt.kodept.traversal.inference.Type.Companion.string
 import ru.tesserakt.kodept.traversal.inference.Type.Companion.tag
 import ru.tesserakt.kodept.traversal.inference.Type.Companion.unit
 import ru.tesserakt.kodept.traversal.inference.Type.Enum
 import kotlin.properties.Delegates
-
-private val logger = KotlinLogging.logger("[Type inference]")
 
 object TypeInferenceAnalyzer : Analyzer() {
     init {
@@ -38,31 +29,7 @@ object TypeInferenceAnalyzer : Analyzer() {
         )
     }
 
-    private data class ExpressionWithStrictParent(val value: AST.Expression) {
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (javaClass != other?.javaClass) return false
-
-            other as ExpressionWithStrictParent
-
-            if (value != other.value) return false
-            if (value.parent != other.value.parent) return false
-
-            return true
-        }
-
-        override fun hashCode(): Int {
-            return 31 * value.hashCode() + value.parent.hashCode()
-        }
-
-        operator fun invoke() = value
-    }
-
-    private var types: MutableMap<ExpressionWithStrictParent, Type> = hashMapOf()
     private val enumTypes = mutableMapOf<AST.EnumDecl, Type>()
-
-    private operator fun AST.Expression.invoke() = ExpressionWithStrictParent(this)
-    private val AST.Expression.type get() = types[this()]!!
 
     private fun AST.TypeReferable.type(): Type = when (this) {
         is AST.EnumDecl.Entry -> tag(name, lazy { enumTypes[parent as AST.EnumDecl]!! })
@@ -98,45 +65,6 @@ object TypeInferenceAnalyzer : Analyzer() {
             else null
         is AST.InferredParameter -> type?.type()
         is AST.InitializedVar -> type?.type()
-    }
-
-    private fun generateEquations(aexpr: AnnotatedExpression) = with(aexpr.expr) {
-        when (this) {
-            is AST.BinaryOperator -> BinaryOperatorDesugaring.contract()
-            is AST.UnaryOperator -> UnaryOperatorDesugaring.contract()
-            is AST.Dereference -> DereferenceEliminator.contract()
-            is AST.ExpressionList -> when (val last = expressions.last()) {
-                is AST.Statement -> TypeEquation(aexpr, unit)
-                is AST.Expression -> TypeEquation(aexpr, last.type)
-            }.nel()
-            is AST.IfExpr -> nonEmptyListOf(TypeEquation(condition, condition.type, bool),
-                el?.body?.let { TypeEquation(aexpr, it.type) } ?: TypeEquation(aexpr, Bottom),
-                TypeEquation(aexpr, body.type)
-            ) + elifs.flatMap {
-                nonEmptyListOf(
-                    TypeEquation(it.condition, it.condition.type, bool), TypeEquation(aexpr, it.body.type)
-                )
-            }
-            is AST.BinaryLiteral -> TypeEquation(aexpr, number).nel()
-            is AST.CharLiteral -> TypeEquation(aexpr, char).nel()
-            is AST.DecimalLiteral -> TypeEquation(aexpr, number).nel()
-            is AST.FloatingLiteral -> TypeEquation(aexpr, floating).nel()
-            is AST.HexLiteral -> TypeEquation(aexpr, number).nel()
-            is AST.OctalLiteral -> TypeEquation(aexpr, number).nel()
-            is AST.StringLiteral -> TypeEquation(aexpr, string).nel()
-            is AST.TupleLiteral -> TypeEquation(aexpr, Tuple(items.map { it.type })).nel()
-            is AST.FunctionCall -> TypeEquation(
-                reference,
-                reference.type,
-                Fn.fromParams(params.map { it.type }, type)
-            ).nel()
-            is AST.ResolvedReference -> referral.type()?.let { TypeEquation(aexpr, it).nel() } ?: emptyList()
-            is AST.ResolvedTypeReference -> TypeEquation(aexpr, referral.type()).nel()
-            is AST.LambdaExpr -> TypeEquation(aexpr, Fn.fromParams(params.map { it.type() ?: T() }, body.type)).nel()
-
-            is AST.Reference -> ReferenceResolver.contract()
-            is AST.TypeReference -> TypeReferenceResolver.contract()
-        }
     }
 
     private fun populateContext(ast: AST) = ast.walkThrough {
