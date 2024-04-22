3e26b335a265a9dfbc03f2ac0677292bf1373f27
ITesserakt <portyas85@mail.ru>
2024-04-22 17:58:43 +0300

    Temp commit

--- 01 - Курсовые проекты/2023-2024 - Разр. механизма вывода типов для компилятора языка Kodept/devenv.nix
+++ 01 - Курсовые проекты/2023-2024 - Разр. механизма вывода типов для компилятора языка Kodept/devenv.nix
@@ -1,7 +1,5 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-    packages = with pkgs; [ texliveFull just daemon ];
-
-    scripts.ide.exec = ''${pkgs.daemon}/bin/daemon ${pkgs.gnome-latex}/bin/gnome-latex'';
+    packages = with pkgs; [ texliveFull just mermaid-cli ];
 }
