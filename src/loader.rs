e0c572032bfa745f21e318b42de1dd217864ad77
ITesserakt <portyas85@mail.ru>
2024-05-13 07:12:47 +0300

    Fixed compilation error in tests

--- src/loader.rs
+++ src/loader.rs
@@ -226,7 +226,7 @@ mod tests {
 
     #[test]
     fn test_load_any_temp_file() {
-        let _ = tempfile();
+        let _ = tempfile::tempfile();
         let loader = Loader::file()
             .with_starting_path(&temp_dir())
             .with_any_source_extension()
