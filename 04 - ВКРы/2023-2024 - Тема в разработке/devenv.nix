245b1f5e5bae7cda0a2c643668accc32fca66508
ITesserakt <portyas85@mail.ru>
2024-06-13 02:38:27 +0300

    Using plantuml instead of mermaid Some fixes to styling

--- 04 - ВКРы/2023-2024 - Тема в разработке/devenv.nix
+++ 04 - ВКРы/2023-2024 - Тема в разработке/devenv.nix
@@ -4,8 +4,14 @@
     packages = with pkgs; [ 
     	texliveFull
     	just
-    	mermaid-cli
     	watchexec
     	inkscape
     ];
+
+    scripts.plantuml.exec = ''
+    	cd $DEVENV_ROOT
+    	${pkgs.jre8}/bin/java -jar "thirdparty/plantuml.jar" -o "$DEVENV_ROOT/src/figures/.generated/" $@
+    '';
+
+    env.GRAPHVIZ_DOT = "${pkgs.graphviz}/bin/dot";
 }
