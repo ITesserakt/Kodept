7c9f5c91f5582827c400f80ea8fd69a6f6600b75
ITesserakt <portyas85@mail.ru>
2024-08-04 18:59:56 +0300

    Fixed errors in tests

--- crates/kodept-parse/src/lexer/mod.rs
+++ crates/kodept-parse/src/lexer/mod.rs
@@ -25,46 +25,42 @@ pub type PegLexer<const TRACE: bool = false> = crate::peg::Lexer<TRACE>;
 pub type PestLexer = crate::pest::Lexer;
 
 #[cfg(test)]
-#[cfg(feature = "nom")]
-#[cfg(not(all()))]
 #[allow(clippy::unwrap_used)]
 mod tests {
     use std::fmt::Debug;
-
-    use nom::Finish;
     use rstest::rstest;
-
-    #[allow(unused_imports)]
-    use crate::lexer::{Ignore::*, token, Token::Ignore};
-    use crate::TokenizationResult;
-
+    use crate::common::TokenProducer;
+    use crate::lexer::{Token::*, Ignore::*, Token, PegLexer};
+    
     #[rstest]
-    #[case::ignore_comment(token("// hello world!"), Ignore(Comment("// hello world!")), None)]
+    #[case::ignore_comment("// hello world!", Comment("// hello world!"), None)]
     #[case::ignore_comment_another_line(
-        token("//hello world!\nthis is not comment"),
-        Ignore(Comment("//hello world!")),
+        "//hello world!\nthis is not comment",
+        Comment("//hello world!"),
         Some("\nthis is not comment")
     )]
     #[case::ignore_multiline_comment(
-        token("/* this is\nmultiline comment */"),
-        Ignore(MultilineComment("/* this is\nmultiline comment */")),
+        "/* this is\nmultiline comment */",
+        MultilineComment("/* this is\nmultiline comment */"),
         None
     )]
     #[case::ignore_multiline_comment_with_rest(
-        token("/* this is\nmultiline comment */ this is not"),
-        Ignore(MultilineComment("/* this is\nmultiline comment */")),
+        "/* this is\nmultiline comment */ this is not",
+        MultilineComment("/* this is\nmultiline comment */"),
         Some(" this is not")
     )]
-    #[case::ignore_newline(token("\n\n\n"), Ignore(Newline), Some("\n\n"))]
-    #[case::ignore_whitespace(token("   \t"), Ignore(Whitespace), None)]
-    fn test_parser<T: PartialEq + Debug>(
-        #[case] result: TokenizationResult<T>,
+    #[case::ignore_newline("\n\n\n", Ignore(Newline), Some("\n\n"))]
+    #[case::ignore_whitespace("   \t", Ignore(Whitespace), None)]
+    fn test_parser<T: PartialEq + Debug + Into<Token<'static>>>(
+        #[case] input: &'static str,
         #[case] expected: T,
         #[case] expected_rest: Option<&'static str>,
     ) {
-        let (rest, data) = result.finish().unwrap();
+        let data = PegLexer::<{crate::TRACING_OPTION}>::new().parse_token(input, 0)
+            .unwrap();
+        let rest = &input[data.span.point.length..];
 
+        assert_eq!(data.token, expected.into());
         assert_eq!(rest, expected_rest.unwrap_or(""));
-        assert_eq!(data, expected);
     }
 }
