78681f6b1cd02b3c845e65d28c8272f3d39fad32
ITesserakt <potryas85@mail.ru>
2025-07-01 21:59:36 +0300

    Added `HierarchicalQuery` into ast crate

--- crates/kodept-ast/src/entity/children.rs
+++ crates/kodept-ast/src/entity/children.rs
@@ -1,8 +1,31 @@
+use crate::arity::{Optional, Plural, Singular};
+use crate::prelude::{ASTNode, NodeId};
+use crate::relationship::NodeRelationship;
+use crate::syntax_tree::children::HasChild;
+use bevy_ecs::prelude::{Entity, Query, RelationshipTarget};
+use bevy_ecs::relationship::Relationship;
+use bevy_ecs::system::SystemParam;
 use derive_more::{Display, Error};
 use smallvec::SmallVec;
 use std::convert::Infallible;
 use std::error::Error;
-use crate::arity::{Optional, Plural, Singular};
+use bevy_ecs::query::QueryFilter;
+
+type Rel<T, U, Tag> = <T as NodeRelationship<U, Tag>>::Relationship;
+type Target<T> = <T as Relationship>::RelationshipTarget;
+
+#[derive(SystemParam)]
+pub struct HierarchicalQuery<'w, 's, T, U, Tag = (), Filter = ()>
+where
+    T: HasChild<U, Tag>,
+    T: ASTNode,
+    U: ASTNode,
+    Tag: 'static,
+    Filter: QueryFilter + 'static
+{
+    parent_query: Query<'w, 's, (Entity, &'static T, &'static Target<Rel<T, U, Tag>>), Filter>,
+    children_query: Query<'w, 's, (Entity, &'static U), Filter>,
+}
 
 pub trait TryFromIter {
     type Container<T>;
@@ -25,6 +48,28 @@ pub enum SingleChildError {
 pub enum OptionChildError {
     #[display("Expected at most one child, found {_0}")]
     AtLeastTwo(#[error(not(source))] usize),
+}
+
+impl<'w, 's, T, U, Tag, Filter> HierarchicalQuery<'w, 's, T, U, Tag, Filter>
+where
+    T: HasChild<U, Tag>,
+    T: ASTNode,
+    U: ASTNode,
+    Tag: 'static,
+    Filter: QueryFilter + 'static
+{
+    pub fn iter(&self) -> impl Iterator<Item = (NodeId<T>, NodeId<U>, &T, &U)> + '_ {
+        self.parent_query.iter().flat_map(|parent| {
+            self.children_query.iter_many(parent.2.iter()).map(move |child| {
+                (
+                    NodeId::from(parent.0),
+                    NodeId::from(child.0),
+                    parent.1,
+                    child.1,
+                )
+            })
+        })
+    }
 }
 
 impl TryFromIter for Singular {
