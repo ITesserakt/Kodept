9b28026fc537c0816d603b6d5cdb4edee1894a27
ITesserakt <potryas85@mail.ru>
2025-10-14 17:41:47 +0300

    Added a way to export AST on bare world

--- crates/kodept-ast/src/syntax_tree/export.rs
+++ crates/kodept-ast/src/syntax_tree/export.rs
@@ -2,12 +2,21 @@ use crate::arity::{Optional, Plural, Singular};
 use crate::prelude::AnyNodeRefItem;
 use crate::properties::{Name, Node, Root};
 use crate::relationship::{ArityValue, Contains, NodeRelationships};
-use bevy_ecs::prelude::{Entity, EntityRef, InMut, Query, Res, Single, With};
+use bevy_ecs::prelude::{
+    Entity, EntityRef, InMut, IntoSystem, Query, ReadOnlySystem, Res, Single, SystemInput, With,
+    World,
+};
 use std::io::Write;
 
 impl super::storage::AST {
+    pub fn export_dot_in<W: Write + 'static>(world: &World, writer: &mut W) -> std::io::Result<()> {
+        run_readonly_system_with(world, extract_system, writer)
+    }
+
     pub fn export_dot<W: Write + 'static>(&mut self, writer: &mut W) -> std::io::Result<()> {
-        self.interact().immediate_with(writer, extract_system)
+        self.interact().immediate_exclusive(|w| {
+            Self::export_dot_in(w, writer)
+        })
     }
 }
 
@@ -25,11 +34,23 @@ fn label<W: Write>(buffer: &mut W, node: AnyNodeRefItem) -> std::io::Result<()> 
     Ok(())
 }
 
+fn run_readonly_system_with<M, In, Out>(
+    world: &World,
+    system: impl IntoSystem<In, Out, M, System: ReadOnlySystem<In = In, Out = Out>>,
+    input: In::Inner<'_>,
+) -> Out
+where
+    In: SystemInput,
+{
+    let mut system = IntoSystem::into_system(system);
+    system.run_readonly(input, world).unwrap()
+}
+
 fn extract_system<W: Write>(
     InMut(buffer): InMut<W>,
     relationships: Res<NodeRelationships>,
     nodes: Query<EntityRef, With<Node>>,
-    root: Single<Entity, With<Root>>
+    root: Single<Entity, With<Root>>,
 ) -> std::io::Result<()> {
     writeln!(buffer, "@startuml")?;
 
