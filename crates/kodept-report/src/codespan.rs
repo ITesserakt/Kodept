b59981cf92e885a5e39f251ab7955c9c9c4ea7d4
ITesserakt <potryas85@mail.ru>
2025-10-15 00:21:04 +0300

    Make `CodespanSettings` clonable by default

--- crates/kodept-report/src/codespan.rs
+++ crates/kodept-report/src/codespan.rs
@@ -1,15 +1,19 @@
+use std::io::Write;
+use std::sync::Arc;
 use crate::files::external::{Error, Files};
 use crate::report::Report;
-use codespan_reporting::term::termcolor::{ColorChoice, StandardStream, WriteColor};
+use codespan_reporting::term::termcolor::{ColorChoice, ColorSpec, StandardStream, WriteColor};
 use codespan_reporting::term::Config;
-use derive_more::Constructor;
 
 pub mod external {
     pub use codespan_reporting::term::{termcolor::ColorChoice, Config, DisplayStyle};
 }
 
-#[derive(Clone, Debug, Constructor)]
-pub struct CodespanSettings<S = StandardStream> {
+#[derive(Debug, Clone)]
+pub struct ClonableStandardStream(Arc<StandardStream>);
+
+#[derive(Clone, Debug)]
+pub struct CodespanSettings<S = ClonableStandardStream> {
     config: Config,
     stream: S,
 }
@@ -28,9 +32,60 @@ pub trait Reportable {
     ) -> Result<(), Error>;
 }
 
+impl<S> CodespanSettings<S> {
+    pub fn new(config: Config, stream: S) -> Self {
+        Self { config, stream }
+    }
+}
+
 impl CodespanSettings {
     pub fn stderr(config: Config, color: ColorChoice) -> Self {
-        Self::new(config, StandardStream::stderr(color))
+        Self::new(config, ClonableStandardStream(Arc::new(StandardStream::stderr(color))))
+    }
+}
+
+impl Write for ClonableStandardStream {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        // fast path
+        if let Some(stream) = Arc::get_mut(&mut self.0) {
+            stream.write(buf)
+        } else {
+            let mut lock = self.0.lock();
+            lock.write(buf)
+        }
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        if let Some(stream) = Arc::get_mut(&mut self.0) {
+            stream.flush()
+        } else {
+            let mut lock = self.0.lock();
+            lock.flush()
+        }
+    }
+}
+
+impl WriteColor for ClonableStandardStream {
+    fn supports_color(&self) -> bool {
+        self.0.supports_color()
+    }
+
+    fn set_color(&mut self, spec: &ColorSpec) -> std::io::Result<()> {
+        if let Some(stream) = Arc::get_mut(&mut self.0) {
+            stream.set_color(spec)
+        } else {
+            let mut lock = self.0.lock();
+            lock.set_color(spec)
+        }
+    }
+
+    fn reset(&mut self) -> std::io::Result<()> {
+        if let Some(stream) = Arc::get_mut(&mut self.0) {
+            stream.reset()
+        } else {
+            let mut lock = self.0.lock();
+            lock.reset()
+        }
     }
 }
 
@@ -72,6 +127,12 @@ impl Settings for &CodespanSettings<StandardStream> {
     fn unpack(&mut self) -> (&Config, impl WriteColor) {
         let lock = self.stream.lock();
         (&self.config, lock)
+    }
+}
+
+impl Settings for &CodespanSettings<ClonableStandardStream> {
+    fn unpack(&mut self) -> (&Config, impl WriteColor) {
+        (&self.config, self.stream.clone())
     }
 }
 
