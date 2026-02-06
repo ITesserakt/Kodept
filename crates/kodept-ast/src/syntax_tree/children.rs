5fe95b15f563dd8e51ac9c6586abb955c694e7d4
ITesserakt <potryas85@mail.ru>
2026-02-06 17:54:58 +0300

    Removed blanket Family impl

--- crates/kodept-ast/src/syntax_tree/children.rs
+++ crates/kodept-ast/src/syntax_tree/children.rs
@@ -1,4 +1,4 @@
-use crate::arity::{Arity, Empty};
+use crate::arity::Arity;
 use crate::prelude::ASTNode;
 use crate::relationship::NodeRelationship;
 use bevy_ecs::prelude::Component;
@@ -38,11 +38,6 @@ pub trait Members<Parent, Tag> {
     type Map<M: Mapper<Self>>;
 }
 pub enum Nothing {}
-
-impl<T: ASTNode> Family<Nothing> for T {
-    type Arity = Empty;
-    type Members = Nothing;
-}
 
 impl<Parent, Tag> Members<Parent, Tag> for Nothing {
     type Map<M: Mapper<Self>> = Nothing;
